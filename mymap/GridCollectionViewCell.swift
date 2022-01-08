7c0206f81a3dd7dade4f139c57a4d9973c1cc5a6
Heejuns <77522822+Heejuns@users.noreply.github.com>
2022-01-08 17:36:26 +0900

    오토레이아웃 적용 완료

---
+++ mymap/GridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GridCollectionViewCell.swift
+//  mymap
+//
+//  Created by 신희준 on 2022/01/08.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
